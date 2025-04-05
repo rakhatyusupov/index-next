aa145bd54ae6c53a9489d5a9f52830a959d5e834
rakhatyusupov <seminar.p2p@gmail.com>
2025-04-05 23:23:11 +0300

    Test build

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,9 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  output: "export",
+  distDir: "docs",
+  basePath: "/index-next",
+};
 
 export default nextConfig;
