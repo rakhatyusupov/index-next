75b470c0b6844be31ad0090603616cde71216ece
rakhatyusupov <seminar.p2p@gmail.com>
2025-04-05 18:22:15 +0300

    init

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,5 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  output: "export",
-  distDir: "docs",
-  basePath: "/index-next",
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
