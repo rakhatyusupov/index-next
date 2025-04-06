d77192444b45c2e4bcfc059d73a6653bcbcd76df
rakhatyusupov <seminar.p2p@gmail.com>
2025-04-06 17:06:06 +0300

    remove doc

--- next.config.ts
+++ next.config.ts
@@ -3,10 +3,10 @@ import type { NextConfig } from "next";
 const isGithubPages = process.env.GITHUB_PAGES === "true";
 
 const nextConfig: NextConfig = {
-  output: "export",
+  /*output: "export",
   distDir: "docs",
   basePath: isGithubPages ? "/index-next" : "",
-  assetPrefix: isGithubPages ? "/index-next/" : "",
+  assetPrefix: isGithubPages ? "/index-next/" : "",*/
 };
 
 export default nextConfig;
