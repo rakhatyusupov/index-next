d7819448d8381b95752e0e4b86725fb671ccf6af
rakhatyusupov <seminar.p2p@gmail.com>
2025-04-06 17:02:05 +0300

    doc test

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,12 @@
 import type { NextConfig } from "next";
 
+const isGithubPages = process.env.GITHUB_PAGES === "true";
+
 const nextConfig: NextConfig = {
-  /* output: "export",
+  output: "export",
   distDir: "docs",
-  basePath: "/index-next",*/
+  basePath: isGithubPages ? "/index-next" : "",
+  assetPrefix: isGithubPages ? "/index-next/" : "",
 };
 
 export default nextConfig;
