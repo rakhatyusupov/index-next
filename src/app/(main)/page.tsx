72a5bdb03d9b983d3097221449abe3abd11b1f78
rakhatyusupov <seminar.p2p@gmail.com>
2025-04-15 20:04:33 +0300

    remove: nav

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,12 +1,5 @@
 import { NavLink } from "@/components/Link/Link";
 
 export default function Home() {
-  return (
-    <>
-      <NavLink href="/cases">кейсы</NavLink>
-      <NavLink href="./studies">стадис</NavLink>
-      <NavLink href="./who">кто</NavLink>
-      <NavLink href="./about">о проекте</NavLink>
-    </>
-  );
+  return <></>;
 }
