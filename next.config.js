a4a8e3a68a62d9547a4b85162656c78c4677feb4
rakhatyusupov <seminar.p2p@gmail.com>
2025-06-22 18:05:11 +0300

    build

--- next.config.js
+++ next.config.js
@@ -7,14 +7,9 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
-  // trailingSlash: true,
 
-  // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
-  // skipTrailingSlashRedirect: true,
-
-  // Optional: Change the output directory `out` -> `dist`
-  // distDir: 'dist',
+  basePath: "/index-next",
+  assetPrefix: "/index-next",
 };
 
 module.exports = nextConfig;
